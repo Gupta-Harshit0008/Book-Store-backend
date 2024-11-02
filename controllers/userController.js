c35a9aa3866965083e9656c5f446a6891897a6d8
harshit gupta <harshitgupta369@gmail.com>
2024-11-02 13:27:14 +0530

    connected userDetails API with database

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,28 @@
-exports.userController=(req,res)=>{
-    console.log(req.body)
-    res.status(200).json({
-      status:'success',
-      message:'user Details fetched',
-    })
+const user=require('../Modals/userModal')
+
+exports.userController=async (req,res)=>{
+  try{
+    email=req.body.email
+const userDetails=await user.findOne({email},'_id userName email isAdmin')
+if(!userDetails){
+  res.status(404).json({
+    status:'failure',
+      message:'no user found',
+  })
+}
+else{
+  res.status(200).json({
+    status:'success',
+    message:'user Details fetched',
+    userDetails
+  })
+}
+    
+  }
+  catch(err){
+    res.status(400).json({
+      status:'failure',
+        message:'Please enter correct details',
+  })
+}
   }
